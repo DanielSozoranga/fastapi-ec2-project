b4f2e5b3bafad6136e3230be9547109b063b3fa9
DanielSozoranga <145289602+DanielSozoranga@users.noreply.github.com>
2025-10-01 13:38:28 -0500

    funciona todo

--- Fast-API v.3/frontend/src/services/api.js
+++ Fast-API v.3/frontend/src/services/api.js
@@ -1,37 +1,28 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080';
+// Para desarrollo local
+const API_BASE_URL = 'ip-172-31-23-133.ec2.internal';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
 export const taskService = {
-  // Obtener todas las tareas
   getTasks: async () => {
     const response = await api.get('/tasks');
     return response.data;
   },
 
-  // Obtener una tarea por ID
-  getTask: async (id) => {
-    const response = await api.get(`/tasks/${id}`);
-    return response.data;
-  },
-
-  // Crear una nueva tarea
   createTask: async (task) => {
     const response = await api.post('/tasks', task);
     return response.data;
   },
 
-  // Actualizar una tarea
   updateTask: async (id, task) => {
     const response = await api.put(`/tasks/${id}`, task);
     return response.data;
   },
 
-  // Eliminar una tarea
   deleteTask: async (id) => {
     const response = await api.delete(`/tasks/${id}`);
     return response.data;
